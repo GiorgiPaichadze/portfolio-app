36b784a16650f5ad4f5b6ba1efba5caa95588f81
georgecrazydev <g.paichadze1996@gmail.com>
2024-02-04 21:30:55 +0400

    replace fetch with axios

--- src/services/http.tsx
+++ src/services/http.tsx
@@ -1,26 +1,26 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
 export const http = async (endpoint: string, method = 'GET', body?: any) => {
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   const url = `${baseURL}${endpoint}`;
 
-  const options = {
+  const config: AxiosRequestConfig = {
     method,
     headers: {
       ...(body ? { 'Content-Type': 'application/json' } : {}),
     },
-    body: body ? JSON.stringify(body) : undefined,
-    // cache: 'no-store' as RequestCache,
+    data: body,
   };
 
   try {
-    const response = await fetch(url, options);
+    const response = await axios(url, config);
 
-    if (!response.ok) {
+    if (response.status !== 200) {
       throw new Error(`HTTP request failed with status ${response.status}`);
     }
 
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error: any) {
     console.error('Error:', error);
     throw new Error(`Failed to fetch data. ${error.message || ''}`);
